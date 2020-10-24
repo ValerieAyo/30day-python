ddfc3489bda442b2693b71f4adf0cc9d28d6c5d3
Valerie <Valerie@Valeries-MacAir.local>
2020-10-24 18:01:40 +0100

    completed day1 task

--- day_1/helloworld.py
+++ day_1/helloworld.py
@@ -1,2 +1,25 @@
-print(2 + 3)
-print(3 // 2)
+# Exercise level 1
+#No 2
+#version Python 2.7
+print (3 + 4)
+print (3 - 4)
+print (3 * 4)
+print (3 % 4)
+print (3 / 4)
+print (3 ** 4)
+print (3 // 4)
+#No 3
+print ("valerie")
+print ("ayo")
+print ("country")
+print ("i am enjoying 30 days of python")
+#No 4
+print (type (10))
+print (type (9.8))
+print (type (3.14))
+print (type (4-4j))
+print (type (['Valerie', 'Python', 'Finland']))
+print (type ('valerie'))
+print (type ('ayo'))
+print (type ('nigeria'))
+#exercise level 2 ...
