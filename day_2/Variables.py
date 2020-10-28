c004eb9a2210cc1c020bce82fef1ccbc37400bcb
Valerie <Valerie@Valeries-MacAir.local>
2020-10-28 23:47:17 +0100

    completed day_2 exercise

---
+++ day_2/Variables.py
@@ -0,0 +1,81 @@
+# #Exercises: level 1
+# #1. Created
+# #2. # 'day 2: 30 days of python programming'
+# #3. 
+
+# first_name = 'Valerie '
+# print('First name: ', first_name)
+# last_name = 'Ayo'
+# print('Last name: ', last_name)
+# full_name = first_name  +  last_name
+# print('Full name: ', full_name)
+# country = 'Wadonski'
+# print('Country: ', country)
+# city = 'lagoon'
+# print('City: ', city)
+# age = 90
+# print('age: ', age )
+# year = 1980
+# print('Year: ', year)
+# is_married = False
+# print('Married?: ', is_married)
+# is_true = 'Yes'
+# print('Do i live in my country: ', is_true)
+# is_light_on = 'No'
+# print('is there power supply: ', is_light_on)
+# my_profile = {'firstname': 'Valerie', 'lastname': 'Ayo', 'Job description':'Human Resource Enthusiast', 'game':'PUBG', 'country':'wadonski', 'city':'lagoon'}
+# print('My profile: ', my_profile)
+# skills = ['administration', 'dancing', 'conversing', 'learning how to learn']# print('first name:', first_name)
+# print ('Hobbies: ', skills)
+# #Exercises: level 2
+# print(type(first_name))
+# print(type(last_name))
+# print(type(full_name))
+# print(type(country))
+# print(type(city))
+# print(type(age))
+# print(type(year))
+# print(type(is_married))
+# print(type(is_true))
+# print(type(is_light_on))
+# print(type(my_profile))
+# print(type(skills))
+# print('first name length:', len(first_name))
+# print('last name length:', len(last_name))
+# num_one = 5 
+# print('num_one', num_one)
+#  num_two = 4 
+# print('num_two', num_two)
+
+#4i to 4vii
+
+# variable_total = num_one + num_two 
+# print('Total: ', variable_total)
+# variable_diff = num_one - num_two
+# variable_product = num_one * num_two
+# variable_divison = num_one / num_two
+# variable_remainder = num_one % num_two
+# variable_exp = num_one ** num_two
+# variable_floor_Division = num_two // num_one
+
+#5i to iii
+
+# radius = 30 
+# pi = 3.142
+# area_of_circle = pi * radius ** 2
+# circum_of_circle = 2 * pi * radius
+# user_radius_input = input('Enter_Radius_of_circle: ')
+# print('area of circle w radius ' + str(user_radius_input) + 'is: ' + str(area_of_circle))
+
+
+#6
+# first_name = input('enter_first_Name: ')
+
+# last_name = input('enter_last_name: ')
+
+# country = input('enter_country: ')
+
+# age = input('enter_age: ')
+
+#7
+#help('keywords')
---
+++ day_3/day_3.py
@@ -0,0 +1,6 @@
+#Exercise_day_3
+#1 
+age = 90
+print('age: ', age)
+my_height = 5.5
+print('my height', my_height)
