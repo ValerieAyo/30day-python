34232124e1e456914dbfaea095cbabfda0e68e95
Valerie <Valerie@Valeries-MacAir.local>
2020-10-29 21:29:47 +0100

    origin

--- day_3/day_3.py
+++ day_3/day_3.py
@@ -1,6 +1,16 @@
 #Exercise_day_3
-#1 
-age = 90
-print('age: ', age)
-my_height = 5.5
-print('my height', my_height)
+# #1 
+# age = 90
+# print('age: ', age)
+# #2
+# my_height = 5.5
+# print('my height: ', my_height,'m')
+# #3
+# complex_variable = 1 + 7j
+# print('complex variable: ', complex_variable)
+# #4
+b =  20
+h = 10
+area_of_triangle = 0.5*b*h
+Calculate_area_of_triangle = input('area of triangle:' )
+
